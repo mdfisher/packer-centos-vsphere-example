8ca4575184e47d74ed5cddb47e16f91360e3c1ee
michael.fisher <michael.fisher@pixomondo.com>
2019-06-08 00:33:09 -0500

    Final fixes for proxy.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+export HTTP_PROXY=http://michael.fisher:<passwd>@proxy.pxm-lax.intra:3128
 export HTTPS_PROXY=http://michael.fisher:<passwd>@proxy.pxm-lax.intra:3128
 
 # Configuring werner-release
 echo "$(date +"%Y-%m-%d %H:%M:%S") - $1" > /etc/werner-release
 
 # Installing epel
-yum --setopt=proxy=http://michael.fisher:<passwd>@proxy.pxm-lax.intra:3128 install -y epel-release
+yum --setopt=proxy=$HTTP_PROXY install -y epel-release
 
 # Installing python-pip
-yum install -y python-pip
+yum --setopt=proxy=$HTTP_PROXY install -y python-pip
 yum remove -y epel-release
-pip install --upgrade pip
+pip install --proxy=$HTTP_PROXY --upgrade pip
 
 # installing Ansible
 pip freeze > /tmp/freeze-begin
-pip install --cache-dir /tmp/cache ansible
+pip install --proxy=$HTTP_PROXY --cache-dir /tmp/cache ansible
 pip freeze > /tmp/freeze-end
